f1bbeb7a22496a06646f44d703c7814f33649b9e
ramshad-av <93275659+ramshad-av@users.noreply.github.com>
2023-06-22 23:19:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function display(val){
+    document.getElementById("textarea").value=document.getElementById("textarea").value+val
+}
+function cleard(){
+    document.getElementById("textarea").value=""
+}
